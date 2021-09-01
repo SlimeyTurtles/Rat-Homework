f3e69992f34ea3d5d758cae957624287172f85ee
avinh <avinhahuynh@gmail.com>
2021-08-31 20:47:59 -0700

    greet works

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
 
+htmlLinkPy='/stub/'
+htmlFile="stub.html"
 
 # connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/avinh/')
+@app.route('/stub/', methods=['GET', 'POST'])
+def stub(htmlLinkPy='/stub/', htmlFile="stub.html"):
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template(htmlFile, greetInput=name, htmlLinkHtml=htmlLinkPy)
+    return render_template(htmlFile, greetInput="World", htmlLinkHtml=htmlLinkPy)
+
+@app.route('/avinh/', methods=['GET', 'POST'])
 def avinh():
-    return render_template("avinh.html")
+    stub('/avinh/', "avinh.html")
 
 @app.route('/akhil/')
 def akhil():
@@ -25,10 +35,6 @@ def calissa():
 @app.route('/valen/')
 def valen():
     return render_template("valen.html")
-
-@app.route('/stub/')
-def stub():
-    return render_template("stub.html")
 
 # runs the application on the development server
 if __name__ == "__main__":
