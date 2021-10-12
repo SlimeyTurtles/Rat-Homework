a44f020a52fa6a1ee154285ec63bfca4c4ba2660
avinh <avinhahuynh@gmail.com>
2021-10-11 17:18:31 -0700

    fixed merge errors

--- main.py
+++ main.py
@@ -76,6 +76,10 @@ def wireframes():
 def logiclab():
     return render_template("/OurWork/logiclab.html")
 
+@app.route('/color-code/')
+def colorcode():
+    return render_template("/OurWork/colorcode-temp.html")
+
 @app.route('/addition/')
 def addition():
     return render_template("/OurWork/addition.html")
